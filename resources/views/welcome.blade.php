d3a8c3989010925642dade216e4feb70bc42e763
iman <marivanaaa97@gmail.com>
2020-12-17 12:22:01 +0100

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+@extends('templates.structure')
+@section('content')
+    @include('about.about')
+    @include('aservice.service')
+    @include('aportfolio.portfolio')
+    @include('atestimonial.testimonial')
+    @include('apricing.pricing')
+    @include('contact.contact')
+
+@endsection
+
+  
+
+    
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+ 
